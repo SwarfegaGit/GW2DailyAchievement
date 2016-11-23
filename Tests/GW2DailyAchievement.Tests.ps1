63b9949f6a4cf0db6226be3a80ade41a699c0861
Andrew <apalmer@xv5.co.uk>
2016-11-23 15:20:59 +0000

    Pester

---
+++ Tests/GW2DailyAchievement.Tests.ps1
@@ -0,0 +1,15 @@
+Describe "Get-GW2DailyAchievement PS$PSVersion Integrations tests" {
+
+    It 'Should not error' {
+        Get-GW2DailyAchievement
+    } | Should Not BeNullOrEmpty
+
+    It 'Should not error' {
+        Get-GW2DailyAchievement -Tomorrow
+    } | Should Not BeNullOrEmpty
+
+     It 'Should not error' {
+        Get-GW2DailyAchievement -Tomorrow | Get-GW2DailyAchievementTip
+    } | Should Not BeNullOrEmpty
+
+}
