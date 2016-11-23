20edb85b7d064df1f955a25bf3845c15b55deb87
Andrew <apalmer@xv5.co.uk>
2016-11-23 16:20:25 +0000

    Added test !Deploy

--- Tests/GW2DailyAchievement.Tests.ps1
+++ Tests/GW2DailyAchievement.Tests.ps1
@@ -6,16 +6,20 @@ Describe "Get-GW2DailyAchievement PS$PSVersion Integrations tests" {
         Get-GW2DailyAchievement | Should Not BeNullOrEmpty
     }
 
-    It 'Should not error' {
+    It 'Tomorrow - Should not error' {
         Get-GW2DailyAchievement -Tomorrow | Should Not BeNullOrEmpty
     }
 
-     It 'Should not error' {
+     It 'Edition and Tip Should not error' {
         Get-GW2DailyAchievement -Edition GuildWars2 | Get-GW2DailyAchievementTip | Should Not BeNullOrEmpty
     }
 
-     It 'Should not error' {
+     It 'Level 21 - Should not error' {
         Get-GW2DailyAchievement -MaxLevel 21 | Should Not BeNullOrEmpty
+    }
+
+     It 'WvW - Should not error' {
+        Get-GW2DailyAchievement -Content WvW | Should Not BeNullOrEmpty
     }
 
 }
