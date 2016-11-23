384d50f501fe9dd419f3fce6ac08c6efe06c4513
Andrew <apalmer@xv5.co.uk>
2016-11-23 15:36:46 +0000

    Fixed broken tests

--- Tests/GW2DailyAchievement.Tests.ps1
+++ Tests/GW2DailyAchievement.Tests.ps1
@@ -3,15 +3,19 @@ Import-Module $PSScriptRoot\..\GW2DailyAchievement -Force
 Describe "Get-GW2DailyAchievement PS$PSVersion Integrations tests" {
 
     It 'Should not error' {
-        Get-GW2DailyAchievement
-    } | Should Not BeNullOrEmpty
+        Get-GW2DailyAchievement | Should Not BeNullOrEmpty
+    }
 
     It 'Should not error' {
-        Get-GW2DailyAchievement -Tomorrow
-    } | Should Not BeNullOrEmpty
+        Get-GW2DailyAchievement -Tomorrow | Should Not BeNullOrEmpty
+    }
 
      It 'Should not error' {
-        Get-GW2DailyAchievement -Tomorrow | Get-GW2DailyAchievementTip
-    } | Should Not BeNullOrEmpty
+        Get-GW2DailyAchievement -Edition GuildWars2 | Get-GW2DailyAchievementTip | Should Not BeNullOrEmpty
+    }
+
+     It 'Should not error' {
+        Get-GW2DailyAchievement -MaxLevel 21 | Should Not BeNullOrEmpty
+    }
 
 }
